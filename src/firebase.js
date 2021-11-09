a4c3e53c3b66f6ab2cc62c5924647cba96f596b3
gauravbasu6 <gauravbasu6@gmail.com>
2021-11-09 09:11:58 +0530

    deleted firebase.js, really

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBpIsu8UxK40-iaONmVSVVcPM7dTiAQE0I",
-    authDomain: "whatsapp-clone-a9610.firebaseapp.com",
-    projectId: "whatsapp-clone-a9610",
-    storageBucket: "whatsapp-clone-a9610.appspot.com",
-    messagingSenderId: "1084743246789",
-    appId: "1:1084743246789:web:a637e0351a01eb32e361ac"
-};
